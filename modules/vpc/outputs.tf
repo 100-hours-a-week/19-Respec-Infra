05b9e9bd20857c2beafbd73cc8708a5e2e43d9f4
김경민 <hdh985@naver.com>
2025-05-14 23:37:21 -0700

    feat: vpc output 파일 추가

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_app_subnets" {
+  value = aws_subnet.private_app[*].id
+}
+
+output "private_db_subnets" {
+  value = aws_subnet.private_db[*].id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
